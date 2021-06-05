c4b785750dc621a2bea899b60f7a43a316db846d
Chetan Bhalerao <Chetan301198@users.noreply.github.com>
2021-06-05 15:27:11 +0530

    Add: Route, Navbar, Footer and Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>chetan</h1>
-      <a className="btn btn-primary">click</a>
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Work" component={Work} />
+          <Route exact path="/About" component={About} />
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 };
